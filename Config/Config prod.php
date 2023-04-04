6f29587bcccd73572070c8517ede6ae7369febd6
Joelgomez <ioelgomez2019@gmail.com>
2023-04-04 10:27:40 -0500

    base de datos original 4

---
+++ Config/Config prod.php
@@ -0,0 +1,36 @@
+<?php 
+	
+	//define("BASE_URL", "http://localhost/tienda_virtual/");
+	const BASE_URL = "https://plan.ugelsanroman.gob.pe/dashboard";
+
+	//Zona horaria
+	//date_default_timezone_set('America/Guatemala');
+	date_default_timezone_set("America/Lima");
+	//date_default_timezone_set('sudamerica/peru');
+	
+
+	//Datos de conexión a Base de Datos
+	const DB_HOST = "localhost";
+	const DB_NAME = "plan_ceplanv1";
+	const DB_USER = "plan_ceplan";
+	const DB_PASSWORD = "by1vW*wQ#k]b";
+	const DB_CHARSET = "utf8";
+
+	//Deliminadores decimal y millar Ej. 24,1989.00
+	const SPD = ".";
+	const SPM = ",";
+
+	//Simbolo de moneda
+	const SMONEY = "Q";
+
+	
+	
+
+
+
+	
+
+
+ ?>
+
+
