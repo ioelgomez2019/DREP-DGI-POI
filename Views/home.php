12dfd87eed1063d7f2aebbdf0782fbae71f1b383
Joelgomez <ioelgomez2019@gmail.com>
2023-04-04 10:11:26 -0500

    base de datos original 2

--- Views/home.php
+++ Views/home.php
@@ -5,6 +5,8 @@
   <title>DREP-CEPLAN</title>
 </head>
 <body>
-    <p>Mini Framework planificacion POI V1.1 - <a href="https://plan.ugelsanroman.gob.pe/dashboard">www.drep.dgi.com</a></p>
+<p>Mini Framework planificacion POI V1.1 web- <a href="http://localhost/DREP-DGI-POI/dashboard">www.drep.dgi.com</a></p>    
+<p>Mini Framework planificacion POI V1.1 local - <a href="https://plan.ugelsanroman.gob.pe/dashboard">www.drep.dgi.com</a></p>
 </body>
 </html>
+
