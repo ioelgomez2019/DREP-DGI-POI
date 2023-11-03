ede1408c94a8105fc7d476759f0c3aee78472157
Joel unaj <ioelgomez2019@gmail.com>
2023-11-03 11:02:51 -0500

    primer commit

--- Config/Config.php
+++ Config/Config.php
@@ -1,19 +1,22 @@
 <?php 
 	
-	//define("BASE_URL", "http://localhost/tienda_virtual/");
-	const BASE_URL = "https://plan.ugelsanroman.gob.pe";
+	define("BASE_URL", "http://localhost/DREP-DGI-POI/");
+	//const BASE_URL = "https://plan.ugelsanroman.gob.pe";**servidor**
 
 	//Zona horaria
-	//date_default_timezone_set('America/Guatemala');
-	date_default_timezone_set("America/Lima");
+	date_default_timezone_set('America/Guatemala');
+	//date_default_timezone_set("America/Lima");
 	//date_default_timezone_set('sudamerica/peru');
 	
 
 	//Datos de conexión a Base de Datos
 	const DB_HOST = "localhost";
-	const DB_NAME = "plan_ceplanv1";
-	const DB_USER = "plan_ceplan";
-	const DB_PASSWORD = "by1vW*wQ#k]b";
+	//const DB_NAME = "plan_ceplanv1";
+	//const DB_USER = "plan_ceplan";
+	//const DB_PASSWORD = "by1vW*wQ#k]b";
+	const DB_NAME = "db_tienda_v6";
+	const DB_USER = "root";
+	const DB_PASSWORD = "";
 	const DB_CHARSET = "utf8";
 
 	//Deliminadores decimal y millar Ej. 24,1989.00
--- Views/home.php
+++ Views/home.php
@@ -5,7 +5,7 @@
   <title>DREP-CEPLAN</title>
 </head>
 <body>   
-<p>Mini Framework planificacion POI V1.1 local - <a href="https://plan.ugelsanroman.gob.pe/dashboard">www.drep.dgi.com</a></p>
+<p>Mini Framework planificacion POI V1.1 local - <a href="http://localhost/DREP-DGI-POI/dashboard">www.drep.dgi.com</a></p>
 </body>
 </html>
 
